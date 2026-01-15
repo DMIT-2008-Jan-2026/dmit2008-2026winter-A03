f91160c94d3b477754a3fd2f67305705498758cb
Joshua188-bit <203899141+Joshua188-bit@users.noreply.github.com>
2026-01-15 09:42:32 -0700

    Review code

--- 01 - javascript review/examples/01 - simple review example/student/js/index.js
+++ 01 - javascript review/examples/01 - simple review example/student/js/index.js
@@ -6,3 +6,37 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+// 1 select form
+let topicForm = document.querySelector(".new-topic-form")
+let list = document.querySelector(".list-group-item");
+
+console.log(topicForm);
+//5. create function to add the text input
+const addTopicToPage = (topicName, topicListElement) => {
+    //6.a) create template string
+    let newTopicElement = `<li class="list-group-item">${topicName}</li>`
+    topicListElement.innerHTML += newTopicElement;
+
+}
+
+
+// 2. add event listener and stop default form submission
+topicForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //3. grab input text & store value
+    let topicInput = e.target.elements["new topic"];
+    let topicText = topicInput.value;
+
+    //4. input validation (no empty strings, use bootstrap classes)
+    if (!topicText) {
+        topicInput.classList.add("is-invalid")
+    } else {
+        topicInput.classList.remove("is-invalid");
+    }
+    //6. call the function
+    addTopicToPage(topicText, list);
+
+    
+    
+});
